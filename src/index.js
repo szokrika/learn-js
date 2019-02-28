f48e1656e23da29f9e4f21b9dbf826aef863c720
Csongor Széles <szokrika@gmail.com>
2019-02-27 23:09:45 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import "./js-primitives.js";
---
+++ src/js-primitives.js
@@ -0,0 +1,8 @@
+var firstName = "John";
+var lastName = "Smith";
+var age = 28;
+var fullAge = true;
+
+var fullName = firstName + ", " + lastName; // string concatenation
+
+console.log(fullAge);
